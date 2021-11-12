781844484fbe36bf02edf4c7330206eafc91cbac
Tousif_Sadeque_Chowdhury <tousifchowdhurybd@gmail.com>
2021-11-12 21:45:39 +0600

    Done

---
+++ Way Too Long Words.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+# include <string>
+using namespace std;
+
+  int main(){
+    int n;
+    string word;
+    cin >>n;
+    for(int i=1;i<=n;i++){
+      cin>> word;
+     if(word.length()<=10){
+       cout<<word<<endl;
+     }
+     else{
+       cout<<word[0]<<word.length()-2<<word[word.length()-1]<<endl;
+     }
+        }
+
+        }
+
+
+
